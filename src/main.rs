56769243e896b6894b99c5e5b849b71698a27d1f
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-08-29 16:10:47 +0800

    Add compression support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bytes::Bytes;
+use futures::AsyncWriteExt;
 use rocket::fairing::AdHoc;
 use rocket::http::ContentType;
 use rocket::response::Responder;
@@ -9,6 +10,125 @@ use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::io;
 use std::path::PathBuf;
+use once_cell::sync::Lazy;
+use maplit::btreemap;
+use futures::AsyncWrite;
+use async_compression::futures::write as encoders;
+
+#[derive(Debug, Clone)]
+enum ArchiveType {
+    None,
+    Tar
+}
+
+impl Default for ArchiveType {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
+impl ArchiveType {
+    async fn archive(&self, b: Bytes) -> Result<Vec<u8>, Error> {
+        match self {
+            ArchiveType::None => Ok(b.to_vec()),
+            ArchiveType::Tar => build_tar(b).await,
+        }
+    }
+}
+
+async fn build_tar(b: Bytes) -> Result<Vec<u8>, Error> {
+    let buffer = Vec::new();
+    let mut ar = async_tar::Builder::new(buffer);
+
+    let mut header = async_tar::Header::new_gnu();
+    header.set_size(b.len() as u64);
+    header.set_mode(0o644);
+    header.set_cksum();
+
+    ar.append_data(&mut header, "data", &b[..])
+        .await
+        .map_err(Error::Io)?;
+
+    ar.into_inner().await.map_err(Error::Io)
+}
+
+#[derive(Debug, Clone)]
+enum CompressionType {
+    None,
+    Brotli,
+    Bzip2,
+    Gzip,
+    Xz,
+    Zstd
+}
+
+impl Default for CompressionType {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
+impl CompressionType {
+    async fn compress(&self, b: Vec<u8>) -> Result<Vec<u8>, Error> {
+        match self {
+            CompressionType::None => Ok(b),
+            algorithm => {
+                let mut encoder = algorithm.encoder().unwrap();
+                encoder.write_all(&b[..]).await.map_err(Error::Io)?;
+                encoder.close().await.map_err(Error::Io)?;
+                // can't move out of trait object
+                // copy the buffer instead
+                Ok(encoder.buffer().to_vec())
+            }
+        }
+    }
+
+    fn encoder(&self) -> Option<Box<dyn Encoder + Send + Unpin>> {
+        use encoders::*;
+        let buffer = Vec::new();
+
+        match self {
+            CompressionType::None => None,
+            CompressionType::Brotli => Some(Box::new(BrotliEncoder::new(buffer))),
+            CompressionType::Bzip2 => Some(Box::new(BzEncoder::new(buffer))),
+            CompressionType::Gzip => Some(Box::new(GzipEncoder::new(buffer))),
+            CompressionType::Xz => Some(Box::new(XzEncoder::new(buffer))),
+            CompressionType::Zstd => Some(Box::new(ZstdEncoder::new(buffer))),
+        }
+    }
+}
+
+trait Encoder : AsyncWrite {
+    fn buffer(&self) -> &[u8];
+}
+
+impl Encoder for encoders::BrotliEncoder<Vec<u8>> {
+    fn buffer(&self) -> &[u8] { self.get_ref() }
+}
+impl Encoder for encoders::BzEncoder<Vec<u8>> {
+    fn buffer(&self) -> &[u8] { self.get_ref() }
+}
+impl Encoder for encoders::GzipEncoder<Vec<u8>> {
+    fn buffer(&self) -> &[u8] { self.get_ref() }
+}
+impl Encoder for encoders::XzEncoder<Vec<u8>> {
+    fn buffer(&self) -> &[u8] { self.get_ref() }
+}
+impl Encoder for encoders::ZstdEncoder<Vec<u8>> {
+    fn buffer(&self) -> &[u8] { self.get_ref() }
+}
+
+static ARCHIVE_TYPES: Lazy<BTreeMap<&'static str, ArchiveType>> = Lazy::new(|| btreemap!{
+    ".tar" => ArchiveType::Tar,
+});
+
+static COMPRESSION_TYPES: Lazy<BTreeMap<&'static str, CompressionType>> = Lazy::new(|| btreemap!{
+    ".br" => CompressionType::Brotli,
+    ".bz2" => CompressionType::Bzip2,
+    ".gz" => CompressionType::Gzip,
+    ".xz" => CompressionType::Xz,
+    ".zst" => CompressionType::Zstd,
+});
 
 #[rocket::get("/<scheme>/<authority>/<path..>?<query..>")]
 async fn index(
@@ -32,11 +152,9 @@ async fn index(
     let path_str = path
         .to_str()
         .ok_or(Error::Simple("invalid path encoding".to_owned()))?;
-    url.set_path(
-        path_str
-            .strip_suffix(".tar")
-            .ok_or(Error::Simple("path must end with \".tar\"".to_owned()))?,
-    );
+    let (path_comp_striped, compression_type) = extract_suffix(path_str, &COMPRESSION_TYPES);
+    let (url_path, archive_type) = extract_suffix(&path_comp_striped, &ARCHIVE_TYPES);
+    url.set_path(&url_path);
     let query_vec: Vec<_> = query
         .into_iter()
         .map(|(l, r)| format!("{}={}", l, r))
@@ -54,24 +172,39 @@ async fn index(
         .await
         .map_err(reqwest_error)?;
 
-    let tar = build_tar(bytes).await?;
-    Ok((ContentType::TAR, tar))
+    let archive = archive_type.archive(bytes).await?;
+    let compressed = compression_type.compress(archive).await?;
+    let content_type = content_type(archive_type, compression_type);
+    Ok((content_type, compressed))
 }
 
-async fn build_tar(b: Bytes) -> Result<Vec<u8>, Error> {
-    let buffer = Vec::new();
-    let mut ar = async_tar::Builder::new(buffer);
+fn extract_suffix<'a, T: Default + Clone>(path: &'a str, map: &BTreeMap<&'static str, T>) -> (&'a str, T) {
+    let mut result_path = None;
+    let mut result_type = None;
 
-    let mut header = async_tar::Header::new_gnu();
-    header.set_size(b.len() as u64);
-    header.set_mode(0o644);
-    header.set_cksum();
+    for (suffix, ty) in map.iter() {
+        match path.strip_suffix(suffix) {
+            Some(stripped) => {
+                result_path = Some(stripped);
+                result_type = Some(ty.clone())
+            },
+            None => continue
+        }
+    }
 
-    ar.append_data(&mut header, "data", &b[..])
-        .await
-        .map_err(Error::Io)?;
+    (result_path.unwrap_or(path), result_type.unwrap_or(Default::default()))
+}
 
-    ar.into_inner().await.map_err(Error::Io)
+fn content_type(a: ArchiveType, c: CompressionType) -> ContentType {
+    match (a, c) {
+        (ArchiveType::Tar, CompressionType::None) => ContentType::TAR,
+        (_, CompressionType::Brotli) => ContentType::new("application", "x-brotli"),
+        (_, CompressionType::Bzip2) => ContentType::new("application", "x-bzip2"),
+        (_, CompressionType::Gzip) => ContentType::GZIP,
+        (_, CompressionType::Xz) => ContentType::new("application", "x-xz"),
+        (_, CompressionType::Zstd) => ContentType::new("application", "zstd"),
+        (_, _) => ContentType::Binary
+    }
 }
 
 #[derive(Deserialize)]
@@ -85,7 +218,7 @@ enum Error {
     Simple(String),
     #[response(status = 400)]
     Request(String),
-    #[response(status = 400)]
+    #[response(status = 500)]
     Io(io::Error),
 }
 
